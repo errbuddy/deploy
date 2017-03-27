924b14694203e9e008c7edf11a1a35a8d71d933a
Philipp Eschenbach <philipp@uberall.com>
2017-03-27 15:16:44 +0200

    fix unstable artifact url

--- start-latest.sh
+++ start-latest.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
 TARGET=/opt/errbuddy.war
-LATEST_RELEASE=$(curl -L -s -H 'Accept: application/json' https://github.com/peh/errbuddy/releases/latest)
-LATEST_VERSION=$(echo $LATEST_RELEASE | sed -e 's/.*"tag_name":"\([^"]*\)".*/\1/')
-ARTIFACT_URL="https://github.com/peh/errbuddy/releases/download/$LATEST_VERSION/errbuddy.war"
+ARTIFACT_URL="https://s3.eu-central-1.amazonaws.com/errbuddy-devel/errbuddy.war"
 
 rm -f $TARGET
 wget -O $TARGET $ARTIFACT_URL
